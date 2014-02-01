694d8d400cea3b899e54f626c7a360afadcd0f2b
Dong Nguyen <kakalos12@gmail.com>
2014-02-01 09:21:06 +0700

    adfasd

--- MandrillMailer.php
+++ MandrillMailer.php
@@ -1,6 +1,27 @@
 <?php
-namespace kakalos12\mandrill ;
 
-class MandrillMailer extends yii\mail\BaseMailer {
+namespace kakalos12\mandrill;
+
+class MandrillMailer extends \yii\mail\BaseMailer {
+
+	public $apiKey = null;
+
+	private $_mailer = null;
+
+	/**
+	 * get the Mandrill instance
+	 *
+	 * @return \Mandrill
+	 */
+	public function getMailer() {
+		if (! is_object ( $this->_mailer ))
+			$this->_mailer = new \Mandrill ( $this->apiKey );
+		return $this->_mailer;
+
+	}
+
+	public function createSubAccount() {
+
+	}
 
 }
